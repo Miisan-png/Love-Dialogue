7a64eb83adb4b537d006f0c78f60e18f575e3a38
Plep-m <menut34@gmail.com>
2024-09-03 20:48:51 +0200

    Feature: improved exemple

--- callbacks/branch1_callback.lua
+++ callbacks/branch1_callback.lua
@@ -1,7 +1,7 @@
 local callbacks = {}
 
 function callbacks.callback()
-    print("branch1_callback HELLLLOOOOOOOOOO")
+    print("You just executed a callback function! by choosing this branch Short dialogue")
 end
 
 return callbacks
---
+++ callbacks/branch2_callback.lua
@@ -0,0 +1,7 @@
+local callbacks = {}
+
+function callbacks.callback()
+    print("You just executed a callback function! by choosing this branch Long dialogue")
+end
+
+return callbacks
